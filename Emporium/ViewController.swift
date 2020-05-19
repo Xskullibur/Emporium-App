5bb4951b650edbcce64b80c485d632ee8964a4c9
Peh Zi Heng <pehziheng@yahoo.com.sg>
2020-05-19 14:52:59 +0800

    Initial Commit

---
+++ Emporium/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Emporium
+//
+//  Created by Peh Zi Heng on 19/5/20.
+//  Copyright © 2020 NYP. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
