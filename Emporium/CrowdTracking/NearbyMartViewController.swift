e841da6eb61b42b735633ce3a38238e61575f6b5
Alson <simasas2222@gmail.com>
2020-06-01 12:37:55 +0800

    Added MAP

---
+++ Emporium/CrowdTracking/NearbyMartViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NearbyMartViewController.swift
+//  Emporium
+//
+//  Created by Xskullibur on 1/6/20.
+//  Copyright © 2020 NYP. All rights reserved.
+//
+
+import UIKit
+
+class NearbyMartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
