57194d1c58962fbbbcf0cdafff5647b24b372099
Xskullibur <alsonproj2222@gmail.com>
2020-06-21 17:32:31 +0800

    Added Queue Template

--- Emporium/ViewControllers/Queue/QueueViewController.swift
+++ Emporium/ViewControllers/Queue/QueueViewController.swift
@@ -7,15 +7,49 @@
 //
 
 import UIKit
+import MaterialComponents.MaterialCards
+import MaterialComponents.MaterialButtons
+import MaterialComponents.MaterialButtons_Theming
 
 class QueueViewController: UIViewController {
 
+    // MARK: - Variable
     var justJoinedQueue = false
+    var store: GroceryStore?
+
+    // MARK: - Outlets
+    @IBOutlet weak var directionBtn: MDCButton!
+    @IBOutlet weak var leaveQueueBtn: MDCButton!
+    @IBOutlet weak var cardView: MDCCard!
     
     
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // User Interface
+        /// Title
+        if let store = store {
+            navigationItem.title = "\(store.name) (\(store.address))"
+        }
+        
+        /// Buttons
+        let containerScheme = MDCContainerScheme()
+        containerScheme.colorScheme.primaryColor = .systemOrange
+        directionBtn.setBorderColor(.systemOrange, for: .normal)
+        directionBtn.setBorderWidth(1, for: .normal)
+        directionBtn.applyOutlinedTheme(withScheme: containerScheme)
+        leaveQueueBtn.applyOutlinedTheme(withScheme: containerScheme)
+        
+        /// CardView
+        cardView.cornerRadius = 13
+        cardView.clipsToBounds = true
+        cardView.setBorderWidth(1, for: .normal)
+        cardView.setBorderColor(UIColor.gray.withAlphaComponent(0.3), for: .normal)
+        cardView.layer.masksToBounds = false
+        cardView.setShadowElevation(ShadowElevation(6), for: .normal)
+        
+        // Volunteer Prompt
         if justJoinedQueue {
             
             let alert = UIAlertController(
