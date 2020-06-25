9f1f948b758cd0e9269c2bbcef956317469c5cdd
Peh Zi Heng <pehziheng2@gmail.com>
2020-06-24 17:34:04 -0700

    Adding MLModel

--- Emporium/ViewControllers/CrowdTracking/CrowdTrackingViewController.swift
+++ Emporium/ViewControllers/CrowdTracking/CrowdTrackingViewController.swift
@@ -6,6 +6,8 @@
 //  Copyright © 2020 NYP. All rights reserved.
 //
 import AVFoundation
+import CoreML
+import Vision
 import UIKit
 import MaterialComponents.MaterialCards
 
@@ -22,6 +24,10 @@ class CrowdTrackingViewController: UIViewController {
     private var deviceInput: AVCaptureDeviceInput!
     private var previewLayer: AVCaptureVideoPreviewLayer!
     private var videoDataOutput: AVCaptureVideoDataOutput!
+    
+    private var bufferSize: CGSize!
+    
+    private var visionModel: VNCoreMLModel!
     
     // MARK: - Lifecycle
     override func viewDidLoad() {
@@ -40,6 +46,8 @@ class CrowdTrackingViewController: UIViewController {
         
         ///Setup camera
         self.setupAVCapture()
+        ///Setup model
+        self.loadModel()
     }
     
     private func setupAVCapture(){
@@ -70,7 +78,7 @@ class CrowdTrackingViewController: UIViewController {
             // Add a video data output
             videoDataOutput.alwaysDiscardsLateVideoFrames = true
             videoDataOutput.videoSettings = [kCVPixelBufferPixelFormatTypeKey as String: Int(kCVPixelFormatType_420YpCbCr8BiPlanarFullRange)]
-//            videoDataOutput.setSampleBufferDelegate(self, queue: videoDataOutputQueue)
+            videoDataOutput.setSampleBufferDelegate(self, queue: videoDataOutputQueue)
         } else {
             print("Could not add video data output to the session")
             session.commitConfiguration()
@@ -82,20 +90,76 @@ class CrowdTrackingViewController: UIViewController {
         captureConnection?.isEnabled = true
         do {
             try  videoDevice!.lockForConfiguration()
-//            let dimensions = CMVideoFormatDescriptionGetDimensions((videoDevice?.activeFormat.formatDescription)!)
-//            bufferSize.width = CGFloat(dimensions.width)
-//            bufferSize.height = CGFloat(dimensions.height)
+            let dimensions = CMVideoFormatDescriptionGetDimensions((videoDevice?.activeFormat.formatDescription)!)
+            bufferSize = CGSize()
+            bufferSize.width = CGFloat(dimensions.width)
+            bufferSize.height = CGFloat(dimensions.height)
             videoDevice!.unlockForConfiguration()
         } catch {
             print(error)
-            
+        }
+        session.commitConfiguration()
+        
         previewLayer = AVCaptureVideoPreviewLayer(session: session)
         previewLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
         previewLayer.frame = previewView.bounds
-        previewView.layer.addSublayer(previewLayer)
+        previewView.layer.insertSublayer(previewLayer, at: 0)
         
+        session.startRunning()
+    }
+    
+    private func loadModel(){
+        let yoloModel = YOLOv3TinyFP16()
+        do {
+        visionModel = try VNCoreMLModel(for: yoloModel.model)
+            
+        let objectRecognition = VNCoreMLRequest(model: visionModel){
+            (request, error) in
+            DispatchQueue.main.async {
+                if let results = request.results {
+                    self.drawVisionRequestResults(results)
+                }
+            }
+            }
+        }catch {
+            print("Unable to create model")
         }
     }
+    
+    private func drawVisionRequestResults(_ results: [Any]) {
+        for observation in results where observation is VNRecognizedObjectObservation {
+            guard let objectObservation = observation as? VNRecognizedObjectObservation else {
+                continue
+            }
+            // Select only the label with the highest confidence.
+            let topLabelObservation = objectObservation.labels[0]
+            let objectBounds = VNImageRectForNormalizedRect(objectObservation.boundingBox, Int(bufferSize.width), Int(bufferSize.height))
+            
+            let shapeLayer = self.createRoundedRectLayerWithBounds(objectBounds)
+            
+//            let textLayer = self.createTextSubLayerInBounds(objectBounds,
+//                                                            identifier: topLabelObservation.identifier,
+//                                                            confidence: topLabelObservation.confidence)
+            //shapeLayer.addSublayer(textLayer)
+            //previewView.layer.insertSublayer(previewLayer, at: 0)
+            previewLayer.addSublayer(shapeLayer)
+        }
+    }
+    
+    private func createRoundedRectLayerWithBounds(_ objectBounds: CGRect)-> CALayer {
+        let shapeLayer = CALayer()
+        shapeLayer.bounds = objectBounds
+        shapeLayer.position = CGPoint(x: objectBounds.midX, y: objectBounds.midY)
+        shapeLayer.name = "Detected Object"
+        shapeLayer.backgroundColor = UIColor.yellow.cgColor
+        return shapeLayer
+        
+    }
+    
+//    private func createTextSubLayerInBounds(_ objectBounds: CGRect, _ identitifer: String, _ confidence: Float) -> CATextLayer {
+//        let textLayer = CATextLayer()
+//        textLayer.name = identitifer
+//    }
     
     /**
         IBAction for stepper, use for manually controlling the amount of shopper inside the supermarket.
