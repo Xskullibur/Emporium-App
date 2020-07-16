f6fd5625004e79c331103320eb435551f768f0ac
Peh Zi Heng <pehziheng@yahoo.com.sg>
2020-07-16 22:16:06 +0800

    Updated Rewards View Controller

--- Emporium/Views/Rewards/MyVouchers/MyVoucherBottomSheetViewController.swift
+++ Emporium/Views/Rewards/MyVouchers/MyVoucherBottomSheetViewController.swift
@@ -24,6 +24,7 @@ class MyVoucherBottomSheetViewController: UIViewController {
 
         // Do any additional setup after loading the view.
         self.updateVoucher()
+        
     }
     
     /**
