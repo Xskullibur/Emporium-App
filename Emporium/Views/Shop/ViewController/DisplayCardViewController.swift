be188f0062c5678effe441c46453b52a7246a103
lee hsien xiang <leehsienx@gmail.com>
2020-07-15 19:52:25 -0700

    fixed cardpayment

--- Emporium/Views/Shop/ViewController/DisplayCardViewController.swift
+++ Emporium/Views/Shop/ViewController/DisplayCardViewController.swift
@@ -115,6 +115,7 @@ class DisplayCardViewController: UIViewController, UITableViewDelegate, UITableV
         paymentInfo.year = 0
         paymentInfo.bank = ""
         paymentInfo.name = ""
+        paymentInfo.userid = ""
         
         for cart in cartData {
             var cartItemAdd = CartItem()
@@ -123,48 +124,53 @@ class DisplayCardViewController: UIViewController, UITableViewDelegate, UITableV
             paymentInfo.cartItems.append(cartItemAdd)
         }
         
-        let data = try? paymentInfo.serializedData()
+        Auth.auth().currentUser?.getIDToken(completion: {
+            token, error in
         
-        let session  = URLSession.shared
-        let url = URL(string: Global.BACKEND_SERVER_HOST + "/cardPayment")
-        var request = URLRequest(url: url!)
-        request.httpMethod = "POST"
-        request.setValue("application/octet-stream", forHTTPHeaderField: "Content-Type")
-        
-        session.uploadTask(with: request, from: data) {
-            data, response, error in
-            if let httpResponse = response as? HTTPURLResponse {
-                
-                if let data = data, let datastring = String(data:data,encoding: .utf8) {
-                    message = datastring
-                }
-                
-                if httpResponse.statusCode == 200 {
-                    DispatchQueue.main.async
-                    {
-                        self.removeSpinner()
-                        let showAlert = UIAlertController(title: "Result", message: "Payment Successful", preferredStyle: .alert)
-                        let back = UIAlertAction(title: "OK", style: .default) {
-                            action in
-                            self.navigationController?.popToRootViewController(animated: true)
+            let data = try? paymentInfo.serializedData()
+            
+            let session  = URLSession.shared
+            let url = URL(string: Global.BACKEND_SERVER_HOST + "/cardPayment")
+            var request = URLRequest(url: url!)
+            request.httpMethod = "POST"
+            request.setValue("Bearer \(token!)", forHTTPHeaderField: "Authorization")
+            request.setValue("application/octet-stream", forHTTPHeaderField: "Content-Type")
+            
+            session.uploadTask(with: request, from: data) {
+                data, response, error in
+                if let httpResponse = response as? HTTPURLResponse {
+                    
+                    if let data = data, let datastring = String(data:data,encoding: .utf8) {
+                        message = datastring
+                    }
+                    
+                    if httpResponse.statusCode == 200 {
+                        DispatchQueue.main.async
+                        {
+                            self.removeSpinner()
+                            let showAlert = UIAlertController(title: "Result", message: "Payment Successful", preferredStyle: .alert)
+                            let back = UIAlertAction(title: "OK", style: .default) {
+                                action in
+                                self.navigationController?.popToRootViewController(animated: true)
+                            }
+                            showAlert.addAction(back)
+                            self.present(showAlert, animated: true, completion: nil)
                         }
-                        showAlert.addAction(back)
-                        self.present(showAlert, animated: true, completion: nil)
                     }
-                }
-                else
-                {
-                    DispatchQueue.main.async
+                    else
                     {
-                        self.removeSpinner()
-                        let showAlert = UIAlertController(title: "Payment Failed", message: message, preferredStyle: .alert)
-                        let cancel = UIAlertAction(title: "OK", style: .cancel)
-                        showAlert.addAction(cancel)
-                        self.present(showAlert, animated: true, completion: nil)
+                        DispatchQueue.main.async
+                        {
+                            self.removeSpinner()
+                            let showAlert = UIAlertController(title: "Payment Failed", message: message, preferredStyle: .alert)
+                            let cancel = UIAlertAction(title: "OK", style: .cancel)
+                            showAlert.addAction(cancel)
+                            self.present(showAlert, animated: true, completion: nil)
+                        }
                     }
                 }
-            }
-        }.resume()
+            }.resume()
+        })
     }
     
     func removeCard(docID: String) {
@@ -180,49 +186,60 @@ class DisplayCardViewController: UIViewController, UITableViewDelegate, UITableV
         paymentInfo.cvc = ""
         paymentInfo.month = 0
         paymentInfo.year = 0
+        
+        //not required
+        paymentInfo.bank = ""
+        paymentInfo.name = ""
+        paymentInfo.userid = ""
+        //not required
 
-        let data = try? paymentInfo.serializedData()
-        
-        let session  = URLSession.shared
-        let url = URL(string: Global.BACKEND_SERVER_HOST + "/removeCard")
-        var request = URLRequest(url: url!)
-        request.httpMethod = "POST"
-        request.setValue("application/octet-stream", forHTTPHeaderField: "Content-Type")
-        
-        session.uploadTask(with: request, from: data) {
-            data, response, error in
-            if let httpResponse = response as? HTTPURLResponse {
-                
-                if let data = data, let datastring = String(data:data,encoding: .utf8) {
-                    message = datastring
-                }
-                
-                if httpResponse.statusCode == 200 {
-                    DispatchQueue.main.async
-                    {
-                        self.removeSpinner()
-                        let showAlert = UIAlertController(title: "Result", message: "Card removed successfully", preferredStyle: .alert)
-                        let doneBtn = UIAlertAction(title: "OK", style: .default) {
-                            action in
-                            self.loadCards()
+        Auth.auth().currentUser?.getIDToken(completion: {
+            token, error in
+            
+            let data = try? paymentInfo.serializedData()
+            
+            let session  = URLSession.shared
+            let url = URL(string: Global.BACKEND_SERVER_HOST + "/removeCard")
+            var request = URLRequest(url: url!)
+            request.httpMethod = "POST"
+            request.setValue("Bearer \(token!)", forHTTPHeaderField: "Authorization")
+            request.setValue("application/octet-stream", forHTTPHeaderField: "Content-Type")
+            
+            session.uploadTask(with: request, from: data) {
+                data, response, error in
+                if let httpResponse = response as? HTTPURLResponse {
+                    
+                    if let data = data, let datastring = String(data:data,encoding: .utf8) {
+                        message = datastring
+                    }
+                    
+                    if httpResponse.statusCode == 200 {
+                        DispatchQueue.main.async
+                        {
+                            self.removeSpinner()
+                            let showAlert = UIAlertController(title: "Result", message: "Card removed successfully", preferredStyle: .alert)
+                            let doneBtn = UIAlertAction(title: "OK", style: .default) {
+                                action in
+                                self.loadCards()
+                            }
+                            showAlert.addAction(doneBtn)
+                            self.present(showAlert, animated: true, completion: nil)
                         }
-                        showAlert.addAction(doneBtn)
-                        self.present(showAlert, animated: true, completion: nil)
                     }
-                }
-                else
-                {
-                    DispatchQueue.main.async
+                    else
                     {
-                        self.removeSpinner()
-                        let showAlert = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
-                        let cancel = UIAlertAction(title: "OK", style: .cancel)
-                        showAlert.addAction(cancel)
-                        self.present(showAlert, animated: true, completion: nil)
+                        DispatchQueue.main.async
+                        {
+                            self.removeSpinner()
+                            let showAlert = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
+                            let cancel = UIAlertAction(title: "OK", style: .cancel)
+                            showAlert.addAction(cancel)
+                            self.present(showAlert, animated: true, completion: nil)
+                        }
                     }
                 }
-            }
-        }.resume()
+            }.resume()
+        })
     }
 }
 
