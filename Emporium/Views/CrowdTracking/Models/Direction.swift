db681e9269ff29be67028e28488042b8977875a0
Peh Zi Heng <pehziheng@yahoo.com.sg>
2020-07-25 15:30:04 +0800

    Added Order manager for testing

---
+++ Emporium/Views/CrowdTracking/Models/Direction.swift
@@ -0,0 +1,14 @@
+//
+//  Direction.swift
+//  Emporium
+//
+//  Created by Peh Zi Heng on 20/7/20.
+//  Copyright © 2020 NYP. All rights reserved.
+//
+
+import Foundation
+
+enum Direction {
+    case leftToRight
+    case rightToLeft
+}
