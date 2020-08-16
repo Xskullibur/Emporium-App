ba34b758b28278875c85413e40b2a1a813901c74
Peh Zi Heng <pehziheng@yahoo.com.sg>
2020-08-16 23:52:07 +0800

    Update protobuf

--- Emporium/Helpers/Global.swift
+++ Emporium/Helpers/Global.swift
@@ -17,9 +17,9 @@ class Global{
     //MARK: Environment Variables IP
     
     //Emporium backend server host
-    //public static let BACKEND_SERVER_HOST: String = ProcessInfo.processInfo.environment["backend_server_host"]!
+    public static let BACKEND_SERVER_HOST: String = ProcessInfo.processInfo.environment["backend_server_host"]!
     //Emporium firebase host
-    //public static let FIREBASE_HOST = ProcessInfo.processInfo.environment["functions_host"]!
+    public static let FIREBASE_HOST = ProcessInfo.processInfo.environment["functions_host"]!
 
     //MARK: END OF Environment Variables IP
     
@@ -27,9 +27,9 @@ class Global{
     //MARK: HARDCODED IP
 
     //Emporium backend server host
-    public static let BACKEND_SERVER_HOST: String = "http://192.168.86.1:5001"
+    //public static let BACKEND_SERVER_HOST: String = "http://192.168.86.1:5001"
     //Emporium firebase host
-    public static let FIREBASE_HOST = "http://192.168.86.1:5000"
+    //public static let FIREBASE_HOST = "http://192.168.86.1:5000"
 
      
     
